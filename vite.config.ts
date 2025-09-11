51279b48fd11ef05a313e9f510bb093f83bc5213
bengo <171782+gobengo@users.noreply.github.com>
2025-09-11 16:55:17 -0700

    vite config uses BASE_URL env for vite base

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
+const base = process.env.BASE_URL ?? undefined
+if (typeof base !== 'undefined') console.debug('vite: using basename from BASE_URL', base)
+
 export default defineConfig({
+  base,
   plugins: [
     tailwindcss(),
   ],
